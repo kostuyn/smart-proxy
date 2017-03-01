f08c989af92242e406ba54ef27cb8e64b74a93d2
Konstantin Altuhov <konstantin.altukhov@onetwotrip.com>
2017-03-01 11:56:34 +0300

    configure ports from ENV

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 'use strict';
 
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
+const connectPort = process.env.CONNECT_PORT || 8001;
+const httpProxyPort = process.env.HTTP_PROXY_PORT || 9001;
+const httpsProxyPort = process.env.HTTPS_PROXY_PORT || 9002;
+const managePort = process.env.MANAGE_PORT || 7001;
+
 const proxyFactory = require('./handlers/proxy');
 const manage = require('./handlers/manage');
 const connectServer = require('./servers/connect-server');
@@ -10,36 +16,7 @@ const configServiceFactory = require('./services/config');
 
 const configService = configServiceFactory();
 
-const httpProxyPort = 9001;
-const httpsProxyPort = 9002;
-
-connectServer(8001, 'localhost', httpProxyPort, httpsProxyPort);
+connectServer(connectPort, 'localhost', httpProxyPort, httpsProxyPort);
 proxyServer.http(httpProxyPort, proxyFactory, configService);
 proxyServer.https(httpsProxyPort, proxyFactory, configService);
-manageServer(7001, manage, configService);
-
-
-const request = require('request');
-process.env.NODE_TLS_REJECT_UNAUTHORIZED = "0";
-setTimeout(function() {
-	request({
-		//url:'https://ott.local/_api/conditions/getBundleConditions/?type=bundles_presearch',
-		url: 'https://module.sletat.ru/Main.svc/GetTourOperators?townFromId=1264&countryId=40',
-		//url: 'https://ott.local/_api/searching/startSync2/?route=2705IEVLON3005&ad=1&cn=0&in=0&cs=E&currency=RUB&source=Spasibo&showProfitParts=true',
-		proxy: 'http://localhost:8001',
-		proxyHeaderWhiteList: ['X-Forwarded-Host', 'X-Forwarded-Prot'],
-		tunnel: true,
-		headers: {
-			host: 'bla-bla',
-			'X-Forwarded-Host': 'module.sletat.ru',
-			'X-Forwarded-Prot': 'https'
-		}
-	}, function(err, response) {
-		if(err) {
-			return console.log(err);
-		}
-
-		console.log('Client!!!!!!!!!!');
-		console.log(response.body);
-	});
-}, 500);
+manageServer(managePort, manage, configService);
